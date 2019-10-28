8f1ba73a549d66b6f67dc05b0fd7a7698b852943
SergiyRezvan <mazyaserg@gmail.com>
2019-10-28 23:00:20 +0200

    Add init script for initial set up

---
+++ db_schema/sql_scripts/directory/init.sql
@@ -0,0 +1,8 @@
+INSERT INTO teacher_roles (id, name) VALUES
+ (nextval('teacher_roles_seq'), 'TEACHER'),
+ (nextval('teacher_roles_seq'), 'MANAGER'),
+ (nextval('teacher_roles_seq'), 'ADMIN');
+
+INSERT INTO teachers (id, first_name, last_name, school_id, email, phone, role_id, username, "password") VALUES
+ (nextval('teachers_seq'), 'Sierhii', 'Riezvan', '1111111', 'mazyaserg@gmail.com', '0999291713',
+ (select id from teachers_seq where name = 'ADMIN'), 'admin', 'admin');
