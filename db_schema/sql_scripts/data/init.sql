cc3dd7207d2c5912b6ebe6c7cd1181d1642e5f7c
SergiyRezvan <mazyaserg@gmail.com>
2019-11-19 00:01:58 +0200

    Add encrypted password

--- db_schema/sql_scripts/data/init.sql
+++ db_schema/sql_scripts/data/init.sql
@@ -5,4 +5,4 @@ INSERT INTO teacher_roles (id, name) VALUES
 
 INSERT INTO teachers (id, first_name, last_name, school_id, email, phone, role_id, username, "password") VALUES
  (nextval('teachers_seq'), 'Sierhii', 'Riezvan', '1111111', 'mazyaserg@gmail.com', '0999291713',
- (select id from teacher_roles where name = 'ADMIN'), 'admin', 'admin');
+ (select id from teacher_roles where name = 'ADMIN'), 'admin', '$2a$10$JRCqf.0YBrTkbnYDjROUS.Eba7E2b406Sdvnu5FM4lJ79/o7d9ypG');
